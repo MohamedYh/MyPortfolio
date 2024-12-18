86b50936460a1ada19a4cc5e118a6ca18071884c
MohamedYh <mohamedadyh00@gmail.com>
2024-12-18 12:58:07 +0100

    ddfs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import Hero from "@/sections/hero";
 import Portfolio from "@/sections/projects";
 import bcklsm from "../../public/bckstl.png";
 import Contact from "@/sections/contact";
-import { useEffect, useRef, useState } from "react";
+import {useEffect, useRef, useState} from "react";
 
 export default function Home() {
     const [offset, setOffset] = useState(0);
@@ -23,15 +23,33 @@ export default function Home() {
         };
 
         window.removeEventListener("scroll", onScroll);
-        window.addEventListener("scroll", onScroll, { passive: true });
+        window.addEventListener("scroll", onScroll, {passive: true});
         return () => window.removeEventListener("scroll", onScroll);
     }, []);
 
+    const cursor = useRef<HTMLDivElement>(null);
+    useEffect(() => {
+        const handleMouseMove = (e: any) => {
+            if (cursor.current) {
+                console.log(e.pageY);
+                cursor.current.style.top = (e.pageY-35)+"px";
+                cursor.current.style.left = (e.pageX-35)+"px";
+            }
+        }
+
+        window.addEventListener("mousemove", handleMouseMove);
+
+        return () => {
+            window.removeEventListener("mousemove", handleMouseMove);
+
+        }
+    },[cursor.current])
+
     return (
-        <div style={{ width: "fit-content", height: "fit-content" }}>
-            <Hero />
-            <Portfolio />
-            <Contact />
+        <div style={{width: "fit-content", height: "fit-content"}}>
+            <Hero/>
+            <Portfolio/>
+            <Contact/>
             <div className="imgcntr">
                 <Image
                     ref={imrf}
@@ -40,6 +58,9 @@ export default function Home() {
                     height={400}
                     alt=""
                 />
+            </div>
+            <div ref={cursor} className="cursor">
+                <div className="point"></div>
             </div>
         </div>
     );
